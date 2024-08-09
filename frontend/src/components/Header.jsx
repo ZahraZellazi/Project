713222ddaf05631ef4c6978dc4012696cc177c54
ZahraZellazi <zahra.zellazi@esprit.tn>
2024-08-09 23:10:47 +0100

    improved header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import { FaSearch } from 'react-icons/fa'; // Import the search icon from react-icons
+import { FaSearch } from 'react-icons/fa';
 
 const Header = () => {
   return (
