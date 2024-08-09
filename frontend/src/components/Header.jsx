2bf97d88029c9d6a126ea1d849b12e5db2811b6b
ZahraZellazi <zahra.zellazi@esprit.tn>
2024-08-09 23:09:52 +0100

    improved header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import { FaSearch } from 'react-icons/fa'; // Import the search icon from react-icons
 
 const Header = () => {
   return (
     <header className="Header">
       <div className="search-bar">
         <input type="text" placeholder="Search..." />
+        <FaSearch className="search-icon" />
       </div>
       <div className="auth-buttons">
         <button>Login</button>
